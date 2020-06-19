372f5d3c8cfca1377258f0b97634e88ec175720a
YashVarshney24 <46783662+YashVarshney24@users.noreply.github.com>
2020-06-19 13:52:11 +0530

    https error

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <h3>MY FIRST AWS CLOUD TASK </h3>
 <?php
    $cloudant_url=`head -n1 path.txt`;
-   $img_path="https://".$cloudant_url."/terraform.png";
+   $img_path="http://".$cloudant_url."/terraform.png";
    echo "<br>";
    echo "<img src='${img_path}' width=100 height=100>";
 ?>
